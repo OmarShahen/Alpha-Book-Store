96680bf1ce8660990b221dcad07da48283c3a7d7
Omar Reda <omarredaelsayedmohamed@gmail.com>
2021-06-07 03:10:49 -0700

    Added firebase authentication sign up

--- public/JS/auth.js
+++ public/JS/auth.js
@@ -1,5 +1,67 @@
 
 
+// Remove confirm password error message
+document.querySelector("#confirmUserPassword").addEventListener("focus", ()=>{
+
+    document.querySelector(".confirm-password-error").style.display="none";
+});
+
+// Remove email error message
+document.querySelector("#userEmail").addEventListener("focus", ()=>{
+    document.querySelector(".email-error").style.display="none";
+});
+
+
+
+
+const signUpForm = document.querySelector("#sign-up-form");
+
+
+signUpForm.addEventListener("submit", (e)=>{
+
+    e.preventDefault();
+
+    const user = {
+        userName: signUpForm["userName"].value,
+        email: signUpForm["userEmail"].value,
+        password: signUpForm["userPassword"].value,
+        confirmedPassword: signUpForm["confirmUserPassword"].value,
+        userPhoneNumber: signUpForm["userPhoneNumber"].value
+    };
+
+    if(user.password != user.confirmedPassword){
+        document.querySelector(".confirm-password-error").style.display = "block";
+        return;
+    }
+
+    auth.createUserWithEmailAndPassword(user.email, user.password)
+    .then(cred => {
+        console.log(cred.user.uid);
+        console.log(cred);
+        signUpForm.reset();
+    })
+    .catch(err=>{
+        document.querySelector(".email-error").style.display="block";
+        document.querySelector(".email-error").innerHTML=err.message;
+    });
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
 
 
 function signIn(){
